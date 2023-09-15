14926bfe3f1ad326c35070c5755864f2180eaeaa
Anup Mor <anupmor1998@gmail.com>
2023-09-15 15:46:37 +0530

    added tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className='text-red-700 text-6xl font-bold text-center mt-10 p-20'>
+      Hello World!
+    </div>
+  );
 }
 
 export default App;
